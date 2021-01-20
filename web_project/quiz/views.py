0af0b2f61513ef5bdb0f2ac6ee91a5214d125e98
Yago Souza Venancio Lima <yagosvl@hotmail.com>
2021-01-19 23:58:08 -0300

    criada as funções que retornam os templates

--- web_project/quiz/views.py
+++ web_project/quiz/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Olá Mundo')
+    return render(request, 'quiz/index_template.html')
+
+def pergunta(request):
+    return render(request, 'quiz/pergunta_template.html')
+
+def score(request):
+    return render(request, 'quiz/score_template.html')
